07549f1679c2ec641acce74c6a4e6cb077bf16b1
cmquintero <cmquinterot@ufpso.edu.co>
2025-07-30 01:27:36 -0500

    Part6 ejercicios6.3-6.8

---
+++ redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -0,0 +1,22 @@
+import { useDispatch } from 'react-redux'
+import { addAnecdote } from '../reducers/anecdoteReducer'
+
+const AnecdoteForm = () => {
+  const dispatch = useDispatch()
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const content = event.target.anecdote.value
+    dispatch(addAnecdote(content))
+    event.target.anecdote.value = ''
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div><input name="anecdote" /></div>
+      <button type="submit">create</button>
+    </form>
+  )
+}
+
+export default AnecdoteForm
